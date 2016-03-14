f527c7be87ae8edcf9cf6fbefbd0e010f71cbc10
simultech <simultech@gmail.com>
2016-03-15 09:49:37 +1000

    Initial Commit

---
+++ Onetrack/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Onetrack
+//
+//  Created by Andrew Dekker on 15/03/2016.
+//  Copyright © 2016 Andrew Dekker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
